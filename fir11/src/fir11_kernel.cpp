500e14ecdef0bfef20f8a2a19f2d44dafd5b91a6
Matthew Schlittler <m.schlittler@wustl.edu>
2024-10-27 00:51:05 +0000

    Updated code to use symmetry

--- fir11/src/fir11_kernel.cpp
+++ fir11/src/fir11_kernel.cpp
@@ -1,4 +1,4 @@
-/*
+ /*
         Filename: fir.cpp
                 FIR lab wirtten for WES/CSE237C class at UCSD.
 
@@ -15,23 +15,26 @@
 extern "C" {
 void fir(data_t *y, data_t x) {
 #pragma HLS pipeline II = 1
-  coef_t c[N] = {53, 0, -91, 0, 313, 500, 313, 0, -91, 0, 53};
+  coef_t c[N] = {53, 0, -91, 0, 313, 500};
 
   // Write your code here
   static data_t shift_reg[N];
   acc_t acc;
   int i;
+  int idx; // Variable to track index in coefficent array
 
   acc = 0;
 Shift_Accum_Loop:
-  for (i = N - 1; i >= 0; i--) {
+  for (i = N - 1, idx = 0; i >= 0; i--) {
     if (i == 0) {
       acc += x * c[0];
       shift_reg[0] = x;
     } else {
       shift_reg[i] = shift_reg[i - 1];
-      acc += shift_reg[i] * c[i];
+      acc += shift_reg[i] * c[idx];
     }
+
+    idx += (i > N/2) ? 1 : -1;
   }
   *y = acc;
 }
